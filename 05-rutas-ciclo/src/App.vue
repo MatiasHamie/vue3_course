dd1ea8079b4e84c02f692bc5ff97fe2cd806d81f
Matias Nahuel Hamie <matias.hamie@devbase.us>
2022-03-14 00:27:10 -0300

    componentes con lazyload

--- 05-rutas-ciclo/src/App.vue
+++ 05-rutas-ciclo/src/App.vue
@@ -1,13 +1,23 @@
 <template>
+  <navbar />
   <img alt="Vue logo" src="./assets/logo.png" />
   <!-- este componente lo usamos directamente y ya esta el router -->
   <router-view />
 </template>
 
 <script>
+// para usar lazyload es como el router 
+// pero necesitamos usar la funcion extra que es
+// defineAsynzComponent
+import { defineAsyncComponent } from "vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Navbar: defineAsyncComponent(() =>
+      import("@/modules/shared/components/Navbar")
+    ),
+  },
 };
 </script>
 
---
+++ 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="navbar-container">
+    <router-link to="/">Pokemon List</router-link>
+    <router-link to="/id">Pokemon por id</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navbar-container {
+  padding: 0 30px;
+}
+
+.navbar-container a {
+  font-weight: bold;
+  color: #2c3e50;
+  margin: 0 10px;
+}
+
+.navbar-container .router-link-exact-active {
+  color: #42b983;
+}
+</style>
