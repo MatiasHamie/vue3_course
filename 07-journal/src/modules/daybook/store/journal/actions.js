8b012d5db5d7fe6b360f4bc936f157ee85d4ad34
Matias Nahuel Hamie <matias.hamie@devbase.us>
2022-04-27 18:24:31 -0300

    vuex configured

---
+++ 07-journal/src/modules/daybook/store/journal/actions.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+export const loadEntries = async (context) => {
+
+}
+// eslint-disable-next-line no-unused-vars
+export const updateEntry = async (context) => {
+
+}
+// eslint-disable-next-line no-unused-vars
+export const createEntry = async (context) => {
+
+}
---
+++ 07-journal/src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(), // 1213231312123
+      date: new Date().toDateString(), // sat 23, julio sarasa
+      text: "Magna amet adipisicing non ipsum voluptate cillum velit. Reprehenderit est eu aliqua ipsum dolor. Aliquip minim pariatur in ullamco incididunt. Eiusmod eu commodo sint Lorem cillum.",
+      picture: null, // https://
+    },
+    {
+      id: new Date().getTime() + 1000, // 1213231312123
+      date: new Date().toDateString(), // sat 23, julio sarasa
+      text: "Laborum aliqua id tempor officia et laborum aliquip. Adipisicing ex sit do est pariatur cupidatat mollit quis nostrud dolore. Ex laboris cillum elit ipsum sunt amet ea ipsum aute. Ullamco qui aute dolore nulla est veniam. Cillum anim ullamco officia ex adipisicing ipsum pariatur ea. Ad veniam nisi et anim excepteur nulla deserunt id officia. Consectetur eu ullamco culpa commodo reprehenderit nostrud.",
+      picture: null, // https://
+    },
+    {
+      id: new Date().getTime() + 2000, // 1213231312123
+      date: new Date().toDateString(), // sat 23, julio sarasa
+      text: "Officia adipisicing ex consequat veniam voluptate elit. Est labore fugiat irure sunt Lorem fugiat anim duis et nostrud ipsum laboris. Exercitation tempor voluptate id minim ut irure nulla dolor ullamco tempor. Minim proident elit minim enim dolor sit ipsum anim. Ea velit mollit id commodo excepteur fugiat tempor qui est tempor. Labore ut ipsum voluptate officia fugiat ullamco est dolore magna. Cupidatat excepteur enim minim cupidatat voluptate occaecat ea amet sint.",
+      picture: null, // https://
+    },
+  ],
+});
